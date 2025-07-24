4587a2f7f7005d8775650d18ea4ac9a6b1ab8e91
Patrick Kelly <31295040+patkelly92@users.noreply.github.com>
2025-07-24 12:38:08 -0400

    update to dark mode again

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className="antialiased flex">
         <Sidebar />
         <main className="flex-1 ml-[var(--sidebar-width)]">
